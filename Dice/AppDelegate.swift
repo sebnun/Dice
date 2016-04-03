8a8e418c103bf9ca453e671760e8060a07db1527
Sebastian <sebnun@gmail.com>
2016-04-03 11:50:18 -0300

    first commit

---
+++ Dice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dice
+//
+//  Created by Sebastian on 4/3/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
