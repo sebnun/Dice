a6ffcfe438429a829f3526dca59791333b6f5d05
Sebastian <sebnun@gmail.com>
2016-05-13 20:32:09 -0300

    fixed, die is being drawn

--- Dice/DieView.swift
+++ Dice/DieView.swift
@@ -9,15 +9,17 @@
 import AppKit
 
 class DieView: NSView {
-    
-    var intValue: Int? = 2 {
+    var intValue: Int? = 6 {
         didSet {
             needsDisplay = true
         }
     }
     
+    override var intrinsicContentSize: NSSize {
+        return NSSize(width: 20, height: 20)
+    }
+    
     override func drawRect(dirtyRect: NSRect) {
-        
         let backgroundColor = NSColor.lightGrayColor()
         backgroundColor.set()
         NSBezierPath.fillRect(bounds)
@@ -25,54 +27,60 @@ class DieView: NSView {
         drawDieWithSize(bounds.size)
     }
     
-    func metricForSize(size: CGSize) -> (edgeLength: CGFloat, dieFrame: CGRect) {
-        
-        let edgeLenght = min(size.width, size.height)
-        let padding = edgeLenght/10.0
-        let drawingBounds = CGRect(x: 0, y: 0, width: edgeLenght, height: edgeLenght)
-        
+    func metricsForSize(size: CGSize) -> (edgeLength: CGFloat, dieFrame: CGRect) {
+        let edgeLength = min(size.width, size.height)
+        let padding = edgeLength/10.0
+        let drawingBounds = CGRect(x: 0, y: 0, width: edgeLength, height: edgeLength)
         let dieFrame = drawingBounds.insetBy(dx: padding, dy: padding)
-        
-        return (edgeLenght, dieFrame)
+        return (edgeLength, dieFrame)
     }
     
     func drawDieWithSize(size: CGSize) {
-        
         if let intValue = intValue {
-            
-            let (edgeLenght, dieFrame) = metricForSize(size)
-            let cornerRadius: CGFloat = edgeLenght/5.0
-            let dotRadius = edgeLenght/12.0
+            let (edgeLength, dieFrame) = metricsForSize(size)
+            let cornerRadius: CGFloat = edgeLength/5.0
+            let dotRadius = edgeLength/12.0
             let dotFrame = dieFrame.insetBy(dx: dotRadius * 2.5, dy: dotRadius * 2.5)
+            
+            NSGraphicsContext.saveGraphicsState()
+            
+            let shadow = NSShadow()
+            shadow.shadowOffset = NSSize(width: 0, height: -1)
+            shadow.shadowBlurRadius = edgeLength/20
+            shadow.set()
             
             NSColor.whiteColor().set()
             NSBezierPath(roundedRect: dieFrame, xRadius: cornerRadius, yRadius: cornerRadius).fill()
             
-            //nested function to make drwaing dots cleaner
-            func drawDot(u: CGFloat, v:CGFloat) {
-                let dotOrigin = CGPoint(x: dotFrame.minX + dotFrame.width * u, y: dotFrame.minY + dotFrame.height * v)
-                let dotRect = CGRect(origin: dotOrigin, size: CGSizeZero).insetBy(dx: -dotRadius, dy: -dotRadius)
+            NSGraphicsContext.restoreGraphicsState()
+            
+            NSColor.blackColor().set()
+            
+            func drawDot(u: CGFloat, _ v: CGFloat) {
+                let dotOrigin = CGPoint(x: dotFrame.minX + dotFrame.width * u,
+                                        y: dotFrame.minY + dotFrame.height * v)
+                let dotRect = CGRect(origin: dotOrigin, size: CGSizeZero)
+                    .insetBy(dx: -dotRadius, dy: -dotRadius)
                 NSBezierPath(ovalInRect: dotRect).fill()
             }
             
             if (1...6).indexOf(intValue) != nil {
-                // Draw the dots:
+                // Draw Dots
                 if [1, 3, 5].indexOf(intValue) != nil {
-                    drawDot(0.5, v: 0.5) // Center dot
+                    drawDot(0.5, 0.5) // center dot
                 }
                 if (2...6).indexOf(intValue) != nil {
-                    drawDot(0, v: 1) // Upper left
-                    drawDot(1, v: 0) // Lower right
+                    drawDot(0, 1) // upper left
+                    drawDot(1, 0) // lower right
                 }
                 if (4...6).indexOf(intValue) != nil {
-                    drawDot(1, v: 1) // Upper right
-                    drawDot(0, v: 0) // Lower left
+                    drawDot(1, 1) // upper right
+                    drawDot(0, 0) // lower left
                 }
                 if intValue == 6 {
-                    drawDot(0, v: 0.5) // Mid left/right
-                    drawDot(1, v: 0.5)
+                    drawDot(0, 0.5) // mid left/right
+                    drawDot(1, 0.5)
                 }
             }
         }
-    }
-}
+    }}
